d973722584466614c32399702f169d07d39fd55a
penoplavaan <penoplavaaan@gmail.com>
2023-02-20 18:16:42 +0500

    update interface namespace

--- app/FeatureToggle/FeatureToggleInterface.php
+++ app/FeatureToggle/FeatureToggleInterface.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace Penoplavaan\FeatureToggleWrapper\FeatureToggle\Interface;
+namespace Penoplavaan\FeatureToggleWrapper\FeatureToggle;
 
 interface FeatureToggleInterface
 {
